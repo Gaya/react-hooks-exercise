a6856118781bcfd6c19b7512c668c18601fa226e
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-26 16:18:34 +0100

    solution in hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Segment,
   Container,
@@ -11,149 +11,129 @@ import 'semantic-ui-css/semantic.min.css';
 
 import './App.css';
 
-class App extends Component {
-  state = {
-    playing: false,
-    timeLeft: 0,
-    currentQuestion: 0,
-    won: false,
-  };
+const questions = [
+  {
+    question: 'What is the answer to life, the universe and everything?',
+    options: ['42', 'Yes', 'There is no answer'],
+    answer: 0,
+  },
+  {
+    question: 'What nationality are the members of the 80\'s pop band "A-ha"?',
+    options: ['Swedish', 'Danish', 'Norwegian'],
+    answer: 2,
+  },
+  {
+    question: 'Who did Han Solo call an "old smoothie"? ',
+    options: ['Chewbacca', 'Lando', 'Luke'],
+    answer: 1,
+  }
+];
 
-  componentDidMount() {
-    this.timerInterval = setInterval(() => {
-      const { playing, timeLeft } = this.state;
+function useTimer(playing) {
+  const [timeLeft, setTimeLeft] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
       if (playing && timeLeft > 0) {
-        this.setState({
-          timeLeft: timeLeft - 1,
-        });
+        setTimeLeft(timeLeft - 1);
       }
     }, 1000);
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.timerInterval);
-  }
+    return () => clearInterval(interval);
+  }, [playing, timeLeft]);
 
-  startGame = () => {
-    this.setState({
-      playing: true,
-      timeLeft: this.timePerQuestion,
-      currentQuestion: 0,
-    })
+  return [timeLeft, setTimeLeft];
+}
+
+function App() {
+  const [playing, setPlaying] = useState(false);
+  const [timeLeft, setTimeLeft] = useTimer(playing);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const timePerQuestion = 10;
+
+  const question = questions[currentQuestion];
+
+  const startGame = () => {
+    setPlaying(true);
+    setTimeLeft(timePerQuestion);
+    setCurrentQuestion(0);
   };
-
-  nextQuestion = () => {
-    const { currentQuestion } = this.state;
-
-    if (currentQuestion + 1 === this.questions.length) {
-      this.setState({
-        playing: false,
-        timeLeft: 0,
-      });
+  const nextQuestion = () => {
+    if (currentQuestion + 1 === questions.length) {
+      setPlaying(false);
+      setTimeLeft(0);
     } else {
-      this.setState({
-        currentQuestion: currentQuestion + 1,
-        timeLeft: this.timePerQuestion,
-      });
+      setTimeLeft(timePerQuestion);
+      setCurrentQuestion(currentQuestion + 1);
     }
   };
-
-  gameOver = () => {
-    this.setState({
-      currentQuestion: 0,
-      playing: false,
-    });
+  const gameOver = () => {
+    setCurrentQuestion(0);
+    setPlaying(false);
   };
 
-  timePerQuestion = 20;
-
-  questions = [
-    {
-      question: 'What is the answer to life, the universe and everything?',
-      options: ['42', 'Yes', 'There is no answer'],
-      answer: 0,
-    },
-    {
-      question: 'What nationality are the members of the 80\'s pop band "A-ha"?',
-      options: ['Swedish', 'Danish', 'Norwegian'],
-      answer: 2,
-    },
-    {
-      question: 'Who did Han Solo call an "old smoothie"? ',
-      options: ['Chewbacca', 'Lando', 'Luke'],
-      answer: 1,
-    }
-  ];
-
-  render() {
-    const { playing, timeLeft, currentQuestion } = this.state;
-
-    const question = this.questions[currentQuestion];
-
-    return (
-      <>
-        <Segment inverted>
-          <Container text>
-            <Header inverted>Awesome trivia game</Header>
-          </Container>
-        </Segment>
-        <Segment vertical>
-          <Container text>
-            {(playing && timeLeft === 0) && (
-              <>
-                <Header>Time is up 😭</Header>
-                <Button
-                  onClick={this.startGame}
-                  content="Start new game"
-                />
-              </>
-            )}
-            {(!playing && timeLeft > 0) && (
-              <Header>Wrong! You lost 🙃</Header>
-            )}
-            {(!playing && currentQuestion > 0) && (
-              <Header>🍕 YOU ARE A WINNER 🍕</Header>
-            )}
-            {!playing && (
-              <>
-                <p>
-                  {currentQuestion > 0 ? 'You won! Play again?' : 'Welcome to the awesome travia game!'}
-                </p>
-                <Button
-                  onClick={this.startGame}
-                  content="Start game"
-                />
-              </>
-            )}
-            {(playing && timeLeft > 0) && (
-              <Card>
-                <Card.Content>
-                  <Card.Header>
-                    {question.question}
-                  </Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  {question.options.map((option, index) => (
-                    <Button
-                      fluid
-                      icon="right arrow"
-                      labelPosition="left"
-                      content={option}
-                      onClick={question.answer === index ? this.nextQuestion : this.gameOver}
-                    />
-                  ))}
-                </Card.Content>
-                <Card.Content extra>
-                  Time left: {timeLeft} seconds
-                </Card.Content>
-              </Card>
-            )}
-          </Container>
-        </Segment>
-      </>
-    );
-  }
+  return (
+    <>
+      <Segment inverted>
+        <Container text>
+          <Header inverted>Awesome trivia game</Header>
+        </Container>
+      </Segment>
+      <Segment vertical>
+        <Container text>
+          {(playing && timeLeft === 0) && (
+            <>
+              <Header>Time is up 😭</Header>
+              <Button
+                onClick={startGame}
+                content="Start new game"
+              />
+            </>
+          )}
+          {(!playing && timeLeft > 0) && (
+            <Header>Wrong! You lost 🙃</Header>
+          )}
+          {(!playing && currentQuestion > 0) && (
+            <Header>🍕 YOU ARE A WINNER 🍕</Header>
+          )}
+          {!playing && (
+            <>
+              <p>
+                {currentQuestion > 0 ? 'You won! Play again?' : 'Welcome to the awesome travia game!'}
+              </p>
+              <Button
+                onClick={startGame}
+                content="Start game"
+              />
+            </>
+          )}
+          {(playing && timeLeft > 0) && (
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  {question.question}
+                </Card.Header>
+              </Card.Content>
+              <Card.Content>
+                {question.options.map((option, index) => (
+                  <Button
+                    fluid
+                    icon="right arrow"
+                    labelPosition="left"
+                    content={option}
+                    onClick={question.answer === index ? nextQuestion : gameOver}
+                  />
+                ))}
+              </Card.Content>
+              <Card.Content extra>
+                Time left: {timeLeft} seconds
+              </Card.Content>
+            </Card>
+          )}
+        </Container>
+      </Segment>
+    </>
+  );
 }
 
 export default App;
